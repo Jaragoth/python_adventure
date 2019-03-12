6ef4e1d5fc1c2c5238a38b7f7b7e8e26b1b119bc
Jaragoth <jaragoth86@gmail.com>
2019-03-11 21:43:58 -0500

    Changed the valid_moves def to return a dictionary with the new locations. Added single digit game starts.

--- main.py
+++ main.py
@@ -66,25 +66,23 @@ def find_finished(current_position, end_location, turns):
 def valid_moves(current_position, maze, maze_size):
     """Function to check your current location and give a list of valid moves"""
 
-    valid_moves_list = []
+    valid_moves_list = dict()
     open_spaces = dict(n=['+  ', '|  ', '   '], s=['+  ', '|  ', '   '], e=['   ', ' • '],
                        w=['   ', '|  ', ' • ', '|• '])
 
     if current_position['y'] != 1 \
-            and maze[current_position['y'] * 2 - 2][current_position['x'] - 1] in open_spaces['n'] \
-            and '#' not in maze[current_position['y'] * 2 - 3][current_position['x'] - 1]:
-        valid_moves_list.append('n')
+            and maze[current_position['y'] * 2 - 2][current_position['x'] - 1] in open_spaces['n']:
+        valid_moves_list['n'] = dict(x=current_position['x'], y=current_position['y']-1)
     if current_position['y'] != maze_size['y'] \
-            and maze[current_position['y'] * 2][current_position['x'] - 1] in open_spaces['s'] \
-            and '#' not in maze[current_position['y'] * 2 + 1][current_position['x'] - 1]:
-        valid_moves_list.append('s')
+            and maze[current_position['y'] * 2][current_position['x'] - 1] in open_spaces['s']:
+        valid_moves_list['s'] = dict(x=current_position['x'], y=current_position['y']+1)
     if current_position['x'] != maze_size['x'] \
             and maze[current_position['y'] * 2 - 1][current_position['x']] in open_spaces['e']:
-        valid_moves_list.append('e')
+        valid_moves_list['e'] = dict(x=current_position['x']+1, y=current_position['y'])
     if current_position['x'] != 1 \
             and maze[current_position['y'] * 2 - 1][current_position['x'] - 2] in open_spaces['w'] \
             and '|' not in maze[current_position['y'] * 2 - 1][current_position['x'] - 1]:
-        valid_moves_list.append('w')
+        valid_moves_list['w'] = dict(x=current_position['x']-1, y=current_position['y'])
 
     return valid_moves_list
 
@@ -94,15 +92,12 @@ def back_it_up(current_position, maze, maze_size):
     pass
 
 
-def process_move(move, current_position, options):
-    if move == 'n' and move in options:
-        current_position['y'] -= 1
-    elif move == 's' and move in options:
-        current_position['y'] += 1
-    elif move == 'e' and move in options:
-        current_position['x'] += 1
-    elif move == 'w' and move in options:
-        current_position['x'] -= 1
+def process_move(move, current_position, options, maze):
+    if move in options.keys():
+        s = maze[current_position['y'] * 2 - 1][current_position['x'] - 1]
+        maze[current_position['y'] * 2 - 1][current_position['x'] - 1] = str.replace(s, '#', ' ')
+        return options[move]
+    return current_position
 
 
 def clear_screen():
@@ -117,12 +112,14 @@ def main():
     while True:
         print("'q' to exit")
         print("Pick a default difficulty: ({})".format("|".join(default_levels.keys())))
-        val = input("Or give us two inputs between 4 and 30 for the height and width of the maze:")
+        val = input("Or give us one or two inputs between 4 and 30 for the height and width of the maze:")
         if val == "q":
             print("Have a nice day!")
             exit(0)
         elif val in default_levels:
             val = default_levels[val]
+        elif len(val.split()) == 1 and str.isdigit(val):
+            val = val + " " + val
         try:
             x, y = [int(x) for x in val.split()]
             if 3 >= x or x >= 31 or 3 >= y or y >= 31:
@@ -138,6 +135,8 @@ def main():
     maze = make_maze(maze_size['x'], maze_size['y'])
     run = 0
     turns = 0
+    last_position = dict()
+    move = ""
     while find_finished(current_position, end_location, turns):
         txt_for_player = "Running {} spaces!".format(run)
         show_me(current_position, maze, end_location, txt_for_player)  # Show the maze
@@ -146,13 +145,15 @@ def main():
         if len(options) == 0:
             print("Game over. Better luck next time.")
             break
-        elif len(options) == 1 and turns != 0:
+        elif len(options) == 2 and turns != 0:
             run += 1
-            move = options[0]
+            for opt in options.keys():
+                if options[opt] != last_position:
+                    move = opt
         else:
             while True:
                 move = input("'q' to quit. Pick a direction ({}): ".format("-".join(options)))
-                if move not in options and move != "q":
+                if move not in options.keys() and move != "q":
                     show_me(current_position, maze, end_location, "That is not a valid option.")
                 else:
                     break
@@ -163,7 +164,8 @@ def main():
             print("Thanks for playing!")
             break
         else:
-            process_move(move, current_position, options)
+            last_position = current_position
+            current_position = process_move(move, current_position, options, maze)
 
 
 if __name__ == '__main__':
